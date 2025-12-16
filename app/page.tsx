b2fc53be806873cc0910f3dfb6e0d5bbbdf5caa0
rohitranjan-2702 <rohitranjan2702@gmail.com>
2025-12-16 22:05:30 +0530

    feat: magik

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import FlippableCard from "@/components/card";
 import SignInSection from "@/components/sign-in-section";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
     // return <SignInSection />;
-    return (window.location.href = "https://fueler.io/shanks");
+    redirect("https://fueler.io/shanks");
   }
 
   return (
